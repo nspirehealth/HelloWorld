734d8ba43a4c9640ac4a498fe0ca8a5e59938d2b
nspirehealth <mvaughn@nspirehealth.com>
2015-09-04 13:10:31 -0600

    My initial commit.

---
+++ nSpire.HelloWorld/nSpire.HelloWorld/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nSpire.HelloWorld
+{
+    /// <summary>
+    /// Some comments. 
+    /// </summary>
+    public class Class1
+    {
+    }
+}
